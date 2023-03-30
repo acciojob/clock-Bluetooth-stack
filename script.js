6d83733b24a045e6c69ac9a4731424c7fc550068
Siddharth Hota <80689111+Bluetooth-stack@users.noreply.github.com>
2023-03-31 00:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// const time = document.querySelector("#time");
+// const date = document.querySelector("#date");
+const dateAndTime = document.querySelector("#timer");
+
+let day = new Date();
+
+// console.log(day.toLocaleString());
+let now = day.toLocaleString();
+dateAndTime.innerHTML = `${now}`;
